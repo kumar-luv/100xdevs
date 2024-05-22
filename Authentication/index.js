0d3cf0b88a32a47eb07083de8657c3b57a7e551b
kumar-luv <luvchaudhary4566@gmail.com>
2024-05-23 01:14:54 +0530

    authentication with database

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,51 +1,97 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const jwtPassword = "123456";
 const app = express();
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://lokeshkumar222803:ZXtyZfhzznyqFNtu@cluster0.5uxnvkq.mongodb.net/"
+);
+const db = mongoose.connection;
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+const User = mongoose.model("User", userSchema);
+app.post("/signup", async function (req, res) {
+  const { username, password } = req.body;
+
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    // Create new user
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    return res.status(201).json({ msg: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+});
 const ALL_USERS = [
-{
-username: "harkirat@gmail.com",
-password: "123",
-name: "harkirat singh",
-},
-{
-username: "raman@gmail.com",
-password: "123321",
-name: "Raman singh",
-},
-{
-username: "priya@gmail.com",
-password: "123321",
-name: "Priya kumari",
-},
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
 ];
 function userExists(username, password) {
-// write logic to return true or false if this user exists
-// in ALL_USERS array
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  let userExist = false;
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (
+      ALL_USERS[i].username === username &&
+      ALL_USERS[i].password === password
+    ) {
+      userExist = true;
+    }
+  }
+  return userExist;
 }
 app.post("/signin", function (req, res) {
-const username = req.body.username;
-const password = req.body.password;
-if (!userExists(username, password)) {
-return res.status(403).json({
-msg: "User doesnt exist in our in memory db",
-});
-}
-var token = jwt.sign({ username: username }, "shhhhh");
-return res.json({
-token,
-});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
 });
 app.get("/users", function (req, res) {
-const token = req.headers.authorization;
-try {
-const decoded = jwt.verify(token, jwtPassword);
-const username = decoded.username;
-// return a list of users other than this username
-} catch (err) {
-return res.status(403).json({
-msg: "Invalid token",
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword);
+    const username = decoded.username;
+    // return a list of users other than this username
+    const otherUsers = ALL_USERS.filter((user) => user.username !== username);
+    return res.json(otherUsers);
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
 });
-}
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
-app.listen(3000)
